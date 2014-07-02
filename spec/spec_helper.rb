1c536c0dcff0d4dde8a43a66c88b4b2b7bb4f953
Dylan <dacrogers@gmail.com>
2014-07-01 23:02:04 -0700

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,10 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # Remove this line if youre not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # If youre not using ActiveRecord, or youd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
@@ -29,4 +29,27 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+end
+
+
+require 'spec_helper'
+describe "Static pages" do
+describe "Home page" do
+it "should have the content 'Sample App'" do
+visit '/static_pages/home'
+page.should have_content('Sample App')
+end
+end
+describe "Help page" do
+it "should have the content 'Help'" do
+visit '/static_pages/help'
+page.should have_content('Help')
+end
+end
+describe "About page" do
+it "should have the content 'About Us'" do
+visit '/static_pages/about'
+page.should have_content('About Us')
+end
+end
 end
